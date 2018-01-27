1f1acd0242ef25c825a721f4596943b70bc5e679
hundeva <hundeva@gmail.com>
2018-01-27 15:01:30 +0100

    hide widgets app shortcut, if desktop is locked

--- src/com/android/launcher3/popup/SystemShortcut.java
+++ src/com/android/launcher3/popup/SystemShortcut.java
@@ -14,6 +14,7 @@ import com.android.launcher3.R;
 import com.android.launcher3.model.WidgetItem;
 import com.android.launcher3.util.PackageUserKey;
 import com.android.launcher3.widget.WidgetsBottomSheet;
+import com.hdeva.launcher.LeanSettings;
 
 import java.util.List;
 
@@ -55,6 +56,10 @@ public abstract class SystemShortcut extends ItemInfo {
         @Override
         public View.OnClickListener getOnClickListener(final Launcher launcher,
                 final ItemInfo itemInfo) {
+            if (LeanSettings.isDesktopLocked(launcher.getApplicationContext())) {
+                return null;
+            }
+
             final List<WidgetItem> widgets = launcher.getWidgetsForPackageUser(new PackageUserKey(
                     itemInfo.getTargetComponent().getPackageName(), itemInfo.user));
             if (widgets == null) {
