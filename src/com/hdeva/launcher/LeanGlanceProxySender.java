d6642fb7e5c64f731f0dc1a31138b3d71b12b30d
hundeva <hundeva@gmail.com>
2018-03-16 12:53:45 +0100

    add a warning if companion app is outdated

--- src/com/hdeva/launcher/LeanGlanceProxySender.java
+++ src/com/hdeva/launcher/LeanGlanceProxySender.java
@@ -2,6 +2,12 @@ package com.hdeva.launcher;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.text.TextUtils;
+import android.widget.Toast;
+
+import com.android.launcher3.R;
 
 public class LeanGlanceProxySender {
 
@@ -10,6 +16,9 @@ public class LeanGlanceProxySender {
 
     private static final String INTENT_EXTRA = "com.hdeva.launcher.INTENT_EXTRA";
     private static final String TARGET_PACKAGE = "com.google.android.apps.nexuslauncher";
+
+    private static final String COMPANION_VERSION_NAME = "1.0.6";
+    private static final int COMPANION_VERSION_CODE = 11;
 
     public static void sendWeatherAction(Context context, Intent weatherIntent) {
         forward(context, AT_A_GLANCE_WEATHER_ACTION, weatherIntent);
@@ -20,10 +29,28 @@ public class LeanGlanceProxySender {
     }
 
     private static void forward(Context context, String action, Intent intentExtra) {
+        verifyCompanionVersion(context);
         Intent intent = new Intent(action);
         intent.setPackage(TARGET_PACKAGE);
         intent.putExtra(INTENT_EXTRA, intentExtra);
         context.sendBroadcast(intent);
+    }
+
+    private static void verifyCompanionVersion(Context context) {
+        String versionName;
+        int versionCode;
+        try {
+            PackageInfo packageInfo = context.getPackageManager().getPackageInfo(TARGET_PACKAGE, 0);
+            versionName = packageInfo.versionName;
+            versionCode = packageInfo.versionCode;
+        } catch (PackageManager.NameNotFoundException e) {
+            versionName = null;
+            versionCode = 0;
+        }
+
+        if (!TextUtils.equals(versionName, COMPANION_VERSION_NAME) || versionCode != COMPANION_VERSION_CODE) {
+            Toast.makeText(context, R.string.lean_glance_not_up_to_date_please_update, Toast.LENGTH_LONG).show();
+        }
     }
 
     private LeanGlanceProxySender() {
