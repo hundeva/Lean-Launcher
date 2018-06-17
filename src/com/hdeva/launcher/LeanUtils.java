506259e8ce14b357fa456596f1dbaba21a63adba
hundeva <hundeva@gmail.com>
2018-06-17 14:21:06 +0200

    change date formatting logic

--- src/com/hdeva/launcher/LeanUtils.java
+++ src/com/hdeva/launcher/LeanUtils.java
@@ -18,11 +18,14 @@ import android.graphics.Point;
 import android.graphics.Rect;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
+import android.icu.text.DateFormat;
+import android.icu.text.DisplayContext;
 import android.net.Uri;
 import android.os.Process;
 import android.os.SystemClock;
 import android.provider.Settings;
 import android.support.v7.graphics.Palette;
+import android.text.format.DateUtils;
 import android.util.Log;
 import android.view.MotionEvent;
 
@@ -36,8 +39,6 @@ import com.android.launcher3.dynamicui.WallpaperColorInfo;
 import com.android.launcher3.util.LooperExecutor;
 import com.google.firebase.crash.FirebaseCrash;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.Locale;
 
 public class LeanUtils {
@@ -277,8 +278,32 @@ public class LeanUtils {
     }
 
     public static String formatDateTime(Context context, long timeInMillis) {
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(LeanSettings.getDateFormat(context), Locale.getDefault());
-        return simpleDateFormat.format(new Date(timeInMillis));
+        try {
+            String format = LeanSettings.getDateFormat(context);
+            String formattedDate;
+            if (Utilities.ATLEAST_NOUGAT) {
+                DateFormat dateFormat = DateFormat.getInstanceForSkeleton(format, Locale.getDefault());
+                dateFormat.setContext(DisplayContext.CAPITALIZATION_FOR_STANDALONE);
+                formattedDate = dateFormat.format(timeInMillis);
+            } else {
+                int flags;
+                if (format.equals(context.getString(R.string.date_format_long))) {
+                    flags = DateUtils.FORMAT_SHOW_WEEKDAY | DateUtils.FORMAT_SHOW_DATE;
+                } else if (format.equals(context.getString(R.string.date_format_normal))) {
+                    flags = DateUtils.FORMAT_SHOW_WEEKDAY | DateUtils.FORMAT_SHOW_DATE | DateUtils.FORMAT_ABBREV_MONTH;
+                } else if (format.equals(context.getString(R.string.date_format_short))) {
+                    flags = DateUtils.FORMAT_SHOW_WEEKDAY | DateUtils.FORMAT_SHOW_DATE | DateUtils.FORMAT_ABBREV_MONTH | DateUtils.FORMAT_ABBREV_WEEKDAY;
+                } else {
+                    flags = DateUtils.FORMAT_SHOW_WEEKDAY | DateUtils.FORMAT_SHOW_DATE | DateUtils.FORMAT_ABBREV_MONTH;
+                }
+
+                formattedDate = DateUtils.formatDateTime(context, timeInMillis, flags);
+            }
+            return formattedDate;
+        } catch (Throwable t) {
+            LeanUtils.reportNonFatal(new Exception("Error formatting At A Glance date", t));
+            return DateUtils.formatDateTime(context, timeInMillis, DateUtils.FORMAT_SHOW_WEEKDAY | DateUtils.FORMAT_SHOW_DATE | DateUtils.FORMAT_ABBREV_MONTH);
+        }
     }
 
     private LeanUtils() {
